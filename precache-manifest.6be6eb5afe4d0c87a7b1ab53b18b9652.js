107e6b4912600c73899dd837301a53078d35d8a7
Artem <bazunc@gmail.com>
2020-04-08 00:04:09 +0500

    Updates

---
+++ precache-manifest.6be6eb5afe4d0c87a7b1ab53b18b9652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8131b14f0f3133a5fc3d582a6d56d2a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7898e9d11889211e4ee1",
+    "url": "/portfolio/static/css/main.53d13216.chunk.css"
+  },
+  {
+    "revision": "c45fc0e4a30c19ce55af",
+    "url": "/portfolio/static/js/2.bae93d64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.bae93d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7898e9d11889211e4ee1",
+    "url": "/portfolio/static/js/main.6e2345ab.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
